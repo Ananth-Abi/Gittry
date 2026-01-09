cd9d51d192582c9529bf31f7d8f7842592905b3a
Ananth-Abi <abiananth50@gmail.com>
2026-01-09 22:21:12 +0530

    new update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-
-
 function App() {
-  
-
   return (
     <>
-      
+      <p className="text-9xl text-amber-800 text">Hi guys</p>
+      <p className="text-primary">Good morning</p>
+      <h1>Hello world</h1>
     </>
   )
 }
